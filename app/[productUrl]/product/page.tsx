0501bf95be2307f5fe148de869fca88892664390
Iñaki Arango <arangoinaki@gmail.com>
2025-02-15 06:29:10 -0800

    Added stepper

--- app/[productUrl]/product/page.tsx
+++ app/[productUrl]/product/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useMemo } from "react";
+
+import { useRouter } from "next/router";
+import { useParams } from "next/navigation";
+
 import { ProductGallery } from "./components/product-gallery";
 import { ProductInfo } from "./components/product-info";
-
-import { Skeleton } from "@/components/ui/skeleton";
 
 const productImages = [
   "https://m.media-amazon.com/images/I/61Ht6pj0JiL._AC_SX695_.jpg",
@@ -25,7 +28,83 @@ const productData = {
   ],
 };
 
-export default function ProductPage() {
+import { Button } from "@/components/ui/button";
+import { defineStepper } from "@/components/ui/stepper";
+
+const {
+  StepperProvider,
+  StepperControls,
+  StepperNavigation,
+  StepperPanel,
+  StepperStep,
+  StepperTitle,
+} = defineStepper(
+  { id: "step-1", title: "Step 1" },
+  { id: "step-2", title: "Step 2" },
+  { id: "step-3", title: "Step 3" },
+);
+
+const Content = ({ id }: { id: string }) => {
+  return (
+    <StepperPanel className="h-[200px] content-center rounded border bg-slate-50 p-8">
+      {" "}
+      <p className="text-xl font-normal">Content for {id}</p>{" "}
+    </StepperPanel>
+  );
+};
+
+export default function StepperDemo() {
+  return (
+    <StepperProvider className="space-y-4" variant="horizontal">
+      {({ methods }) => (
+        <>
+          <StepperNavigation>
+            {methods.all.map((step) => (
+              <StepperStep
+                key={step.id}
+                of={step.id}
+                onClick={() => methods.goTo(step.id)}
+              >
+                <StepperTitle>{step.title}</StepperTitle>
+              </StepperStep>
+            ))}
+          </StepperNavigation>
+          {methods.switch({
+            "step-1": (step) => <Content id={step.id} />,
+            "step-2": (step) => <Content id={step.id} />,
+            "step-3": (step) => <Content id={step.id} />,
+          })}
+          <StepperControls>
+            {!methods.isLast && (
+              <Button
+                variant="secondary"
+                onClick={methods.prev}
+                disabled={methods.isFirst}
+              >
+                {" "}
+                Previous{" "}
+              </Button>
+            )}
+            <Button onClick={methods.isLast ? methods.reset : methods.next}>
+              {methods.isLast ? "Reset" : "Next"}
+            </Button>{" "}
+          </StepperControls>{" "}
+        </>
+      )}
+    </StepperProvider>
+  );
+}
+
+export function ProductPage() {
+  // const router = useRouter();
+  const params = useParams<{ productUrl: string }>();
+
+  const productUrl = useMemo(
+    () => decodeURIComponent(params.productUrl),
+    [params],
+  );
+  console.log(productUrl);
+
   const handleAddToCart = () => console.log("HELLOOO");
 
   return (
